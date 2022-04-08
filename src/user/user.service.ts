b833f91c5500f844e5fcee32e74a66eaa143af2e
Evgheni Calcutin <evgheni.calcutin@gmail.com>
2022-04-08 16:18:22 +0300

    Bootstrap single user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -6,7 +6,23 @@ import { UserRepository } from "./user.repository";
 
 @Injectable()
 export class UserService {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(private readonly userRepository: UserRepository) {
+    setImmediate(() => {
+      this.boot();
+    });
+  }
+
+  async boot(): Promise<void> {
+    const user = await this.userRepository.findOne({
+      username: "root",
+    });
+    if (!user) {
+      await this.userRepository.create({
+        username: "root",
+        password: "root",
+      });
+    }
+  }
 
   async getUsers(): Promise<User[]> {
     return this.userRepository.find({});
